3b4be2ea4075d0926c2a0495a970a112ccdf22c8
Fayas <thefayasnoushad@gmail.com>
2025-10-05 14:12:40 +0530

    add Fibonacci program

---
+++ Fibonacci.java
@@ -0,0 +1,25 @@
+// Write the java program to display Fibonacci series up to a limit.
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String args[]) {
+        try (Scanner scan = new Scanner(System.in)) {
+            int limit, first = 0, second = 1, next;
+            System.out.print("Enter a limit for fibonacci series:- ");
+            limit = scan.nextInt();
+            System.out.print("Fibonacci Series:");
+            for (int i = 0; i <= limit; i++) {
+                if (i < 2)
+                    next = i;
+                else {
+                    next = first + second;
+                    first = second;
+                    second = next;
+                }
+                System.out.print("  " + next);
+            }
+            System.out.println();
+        }
+    }
+}
